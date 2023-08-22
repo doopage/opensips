172888027d62ce30067ee08e12689be856b1b321
truong.hua <truong.hua@youthdev.net>
2023-08-22 23:02:09 +0700

    Support to use SQL database as a cahedb database, so now federate cluster of usrloc can use a SQL database to replace Cassandra

--- modules/cachedb_sql/cachedb_sql.c
+++ modules/cachedb_sql/cachedb_sql.c
@@ -38,6 +38,7 @@
 #include "../../timer.h"
 #include "../../mem/mem.h"
 #include "../../mem/shm_mem.h"
+#include "../../lib/osips_malloc.h"
 
 typedef struct {
 	struct cachedb_id *id;
@@ -46,6 +47,7 @@ typedef struct {
 
 	db_con_t* cdb_db_handle;
 	db_func_t cdb_dbf;
+    str table;
 } cachedbsql_con;
 
 #define MAX_RAW_QUERY_SIZE	512
@@ -66,6 +68,7 @@ static void destroy(void);
 #define COUNTER_VALUE_COL_LEN (sizeof(COUNTER_VALUE_COL) - 1)
 #define EXPIRES_COL "expires"
 #define EXPIRES_COL_LEN (sizeof(EXPIRES_COL) - 1)
+#define MAX_NUM_COL 16
 
 #define CACHEDB_SQL_TABLE_VERSION	2
 
@@ -149,10 +152,10 @@ cachedbsql_con* dbcache_new_connection(struct cachedb_id* id)
 			db_url.s += (p-id->group_name) + 1;
 			db_url.len -= (p-id->group_name) + 1;
 			break;
-		}		
+		}
 	}
-
-	con = pkg_malloc(sizeof(cachedbsql_con));
+    
+    con = pkg_malloc(sizeof(cachedbsql_con));
 	if(con == NULL) {
 		LM_ERR("no more pkg\n");
 		return 0;
@@ -161,6 +164,22 @@ cachedbsql_con* dbcache_new_connection(struct cachedb_id* id)
 	memset(con,0,sizeof(cachedbsql_con));
 	con->id = id;
 	con->ref = 1;
+    
+    for (p = db_url.s + db_url.len - 1; p > db_url.s; p--) {
+        if (*p == '/')
+            break;
+        // allow to specify table name by syntax "mysql://user:pass@domain:3306/database.table"
+		if (*p == '.') {
+            con->table.s = p + 1;
+            con->table.len = db_url.len - (p - db_url.s) - 1;
+			db_url.len = p - db_url.s;
+			break;
+		}
+	}
+    
+    if (!con->table.s) {
+        con->table = db_table;
+    }
 
 	if (db_bind_mod(&db_url, &con->cdb_dbf) < 0){
 		LM_ERR("unable to bind to a database driver\n");
@@ -209,6 +228,7 @@ static void dbcache_destroy(cachedb_con *con)
 
 #define CACHEDBSQL_CON(c)  (((cachedbsql_con*)((c)->data))->cdb_db_handle)
 #define CACHEDBSQL_FUNC(c) (((cachedbsql_con*)((c)->data))->cdb_dbf)
+#define CACHEDBSQL_TABLE(c) (((cachedbsql_con*)((c)->data))->table)
 
 static int dbcache_set(cachedb_con *con, str* attr, str* value, int expires)
 {
@@ -332,29 +352,30 @@ out_err:
 	return -1;
 }
 
-static int dbcache_remove(cachedb_con *con, str* attr)
+static int _dbcache_remove(cachedb_con *con, str* attr, const str *key)
 {
-	db_key_t key;
 	db_val_t val;
-
-	key = &key_column;
 
 	val.type = DB_STR;
 	val.nul = 0;
 	val.val.str_val.s = attr->s;
 	val.val.str_val.len = attr->len;
 
-	if (CACHEDBSQL_FUNC(con).use_table(CACHEDBSQL_CON(con), &db_table) < 0) {
+	if (CACHEDBSQL_FUNC(con).use_table(CACHEDBSQL_CON(con), &CACHEDBSQL_TABLE(con)) < 0) {
 		LM_ERR("sql use_table failed\n");
 		return -1;
 	}
 
-	if (CACHEDBSQL_FUNC(con).delete(CACHEDBSQL_CON(con), &key, 0, &val, 1) < 0) {
+	if (CACHEDBSQL_FUNC(con).delete(CACHEDBSQL_CON(con), (const db_key_t*)&key, 0, &val, 1) < 0) {
 		LM_ERR("deleting from database failed\n");
 		return -1;
 	}
 
 	return 0;
+}
+
+static int dbcache_remove(cachedb_con *con, str* attr) {
+    return _dbcache_remove(con, attr, &key_column);
 }
 
 static int dbcache_add(cachedb_con *con, str *attr, int val, int expires, int *new_val)
@@ -510,6 +531,377 @@ static void dbcache_clean(unsigned int ticks, void* param)
 		pkg_free(lst);
 }
 
+static int append_dbcache_val_to_dict(const db_val_t *value, cdb_dict_t *cdb_dict,
+									cdb_key_t *cdb_key)
+{
+	cdb_pair_t *pair;
+
+	pair = cdb_mk_pair(cdb_key, NULL);
+	if (!pair) {
+		LM_ERR("oom\n");
+		return -1;
+	}
+
+	if (VAL_NULL(value)) {
+		pair->val.type = CDB_NULL;
+		cdb_dict_add(pair, cdb_dict);
+		return 0;
+	}
+
+	switch (VAL_TYPE(value)) {
+	case DB_STR:
+        pair->val.type = CDB_STR;
+        if (pkg_str_dup(&pair->val.val.st, &VAL_STR(value)) < 0)
+            goto error;
+		break;
+    case DB_STRING:
+        pair->val.type = CDB_STR;
+        pair->val.val.st.len = strlen(VAL_STRING(value));
+        pair->val.val.st.s = pkg_malloc(pair->val.val.st.len);
+
+        if (pair->val.val.st.s == NULL) {
+            LM_ERR("no more pkg\n");
+            goto error;
+        }
+        memcpy(pair->val.val.st.s, VAL_STRING(value), pair->val.val.st.len);
+        
+		break;
+	case DB_INT:
+		pair->val.type = CDB_INT32;
+		pair->val.val.i32 = VAL_INT(value);
+		break;
+	default:
+		LM_ERR("Unsupported SQL data type: %d\n", VAL_TYPE(value));
+		return -1;
+	}
+
+	cdb_dict_add(pair, cdb_dict);
+
+	return 0;
+
+error:
+	pkg_free(pair);
+	return -1;
+}
+
+static int dbcache_result_to_cdb_res(const db_res_t *res, cdb_res_t *cdb_res)
+{
+	db_row_t* row;
+	db_val_t *val;
+	cdb_row_t *cdb_row = NULL;
+	cdb_key_t cdb_key;
+	int col_idx;
+    int i;
+    int nrows = RES_ROW_N(res);
+    int ncols = RES_COL_N(res);
+
+	cdb_res_init(cdb_res);
+
+    for(i = 0; i < nrows; i++) {
+		cdb_row = pkg_malloc(sizeof *cdb_row);
+		if (!cdb_row) {
+			LM_ERR("oom\n");
+			goto error;
+		}
+
+		INIT_LIST_HEAD(&cdb_row->dict);
+        
+        row = RES_ROWS(res) + i;
+
+		for (col_idx = 0; col_idx < ncols; col_idx++) {
+            cdb_key.name = **(RES_NAMES(res) + col_idx);
+			if (col_idx == 0)
+				cdb_key.is_pk = 1;
+			else
+				cdb_key.is_pk = 0;
+
+			val = ROW_VALUES(row) + col_idx;
+			
+            if (append_dbcache_val_to_dict(val, &cdb_row->dict, &cdb_key) < 0) {
+				LM_ERR("Failed to add column to cdb result\n");
+				cdb_free_entries(&cdb_row->dict, osips_pkg_free);
+				goto error;
+			}
+		}
+
+		cdb_res->count++;
+		list_add_tail(&cdb_row->list, &cdb_res->rows);
+	}
+
+	return 0;
+
+error:
+	if (cdb_row)
+		pkg_free(cdb_row);
+	return -1;
+}
+
+/*
+ * query function
+ */
+static int dbcache_col_query(cachedb_con *con, const cdb_filter_t *filter,
+						cdb_res_t *cdb_res)
+{
+    db_key_t key[MAX_NUM_COL];
+    db_op_t op[MAX_NUM_COL];
+	db_val_t val[MAX_NUM_COL];
+    int nkey = 0;
+	db_res_t* db_res = NULL;
+    const cdb_filter_t *f;
+
+	if (CACHEDBSQL_FUNC(con).use_table(CACHEDBSQL_CON(con), &CACHEDBSQL_TABLE(con)) < 0) {
+		LM_ERR("sql use_table failed\n");
+		return -1;
+	}
+    
+	for (f = filter; f; f = f->next) {
+        if (nkey >= MAX_NUM_COL) {
+            LM_BUG("Maximum number of filter supported is %d\n", MAX_NUM_COL);
+        }
+        
+        key[nkey] = &f->key.name;
+        
+        switch(f->op) {
+        case CDB_OP_EQ:
+			op[nkey] = OP_EQ;
+			break;
+		case CDB_OP_LT:
+			op[nkey] = OP_LT;
+			break;
+		case CDB_OP_LTE:
+			op[nkey] = OP_LEQ;
+			break;
+		case CDB_OP_GT:
+			op[nkey] = OP_GT;
+			break;
+		case CDB_OP_GTE:
+			op[nkey] = OP_GEQ;
+			break;
+		default:
+			LM_BUG("unsupported operator: %d\n", f->op);
+			return -1;
+        }
+        
+        if (f->val.is_str) {
+			val[nkey].type          = DB_STR;
+            val[nkey].nul           = 0;
+            val[nkey].val.str_val   = f->val.s;
+		} else {
+			val[nkey].type          = DB_INT;
+            val[nkey].nul           = 0;
+            val[nkey].val.int_val   = f->val.i;
+		}
+        
+        nkey++;
+    }
+
+	if (CACHEDBSQL_FUNC(con).query(CACHEDBSQL_CON(con), key, op, val, NULL, nkey, 0, NULL, &db_res) < 0) {
+		LM_ERR("failed to query database\n");
+		return -1;
+	}
+
+	if (db_res == NULL) {
+		LM_DBG("failed to query database\n");
+		return -2;
+	}
+    
+    if (dbcache_result_to_cdb_res(db_res, cdb_res) < 0) {
+        if (CACHEDBSQL_FUNC(con).free_result(CACHEDBSQL_CON(con), db_res) < 0)
+            LM_DBG("failed to free result of query\n");
+        return -1;
+    }
+    
+    if (CACHEDBSQL_FUNC(con).free_result(CACHEDBSQL_CON(con), db_res) < 0) {
+		LM_DBG("failed to free result of query\n");
+        return -1;
+    }
+    
+    return 0;
+}
+
+static int dbcache_truncate(cachedb_con *con)
+{
+    char buf[128];
+    int buf_len;
+    str sql;
+    
+    if (CACHEDBSQL_FUNC(con).use_table(CACHEDBSQL_CON(con), &CACHEDBSQL_TABLE(con)) < 0) {
+		LM_ERR("sql use_table failed\n");
+		return -1;
+	}
+
+	buf_len = snprintf(buf, 128, "TRUNCATE %.*s", db_table.len, db_table.s);
+    
+    sql.s = buf;
+    sql.len = buf_len;
+
+	if (buf_len < 0) {
+		LM_ERR("Failed to build query string for cachedb 'truncate'\n");
+		return -1;
+	}
+
+	if (CACHEDBSQL_FUNC(con).raw_query(CACHEDBSQL_CON(con), &sql, NULL) < 0) {
+		LM_ERR("failed to query database\n");
+		return -1;
+	}
+    
+    return 0;
+}
+
+static int dbcache_col_update(cachedb_con *con, const cdb_filter_t *row_filter,
+						const cdb_dict_t *pairs)
+{
+    db_key_t key[MAX_NUM_COL], set_key[MAX_NUM_COL];
+    db_op_t op[MAX_NUM_COL];
+	db_val_t val[MAX_NUM_COL], set_val[MAX_NUM_COL];
+    int n_filter = 0, n_set = 0, n_equal = 0;
+    const cdb_filter_t *f;
+    int max_ttl = 0;
+    struct list_head *_;
+    cdb_pair_t *pair;
+    char is_complex_filter = 0;
+
+	if (!con) {
+		LM_ERR("null parameter\n");
+		return -1;
+	}
+    
+    if (!row_filter) {
+		LM_ERR("Updating all the rows at once is not supported\n");
+		return -1;
+	}
+
+	if (CACHEDBSQL_FUNC(con).use_table(CACHEDBSQL_CON(con), &CACHEDBSQL_TABLE(con)) < 0) {
+		LM_ERR("sql use_table failed\n");
+		return -1;
+	}
+    
+    list_for_each (_, pairs) {
+        pair = list_entry(_, cdb_pair_t, list);
+        if (!pair->key.name.s)
+            continue;
+
+        if (pair->ttl > max_ttl)
+            max_ttl = pair->ttl;
+        
+        if (n_set >= MAX_NUM_COL) {
+            LM_BUG("Maximum number of update column supported is %d\n", MAX_NUM_COL);
+        }
+
+        if (pair->subkey.s) {
+            LM_BUG("Cachedb SQL does not support subkey\n");
+        } else {
+            set_key[n_set] = &pair->key.name;
+            switch (pair->val.type) {
+            case CDB_NULL:
+                set_val[n_set].nul = 1;
+                break;
+            case CDB_INT32:
+                set_val[n_set].type         = DB_INT;
+                set_val[n_set].nul          = 0;
+                set_val[n_set].val.int_val  = pair->val.val.i32;
+                break;
+            case CDB_INT64:
+                set_val[n_set].type             = DB_INT;
+                set_val[n_set].nul              = 0;
+                set_val[n_set].val.bigint_val   = pair->val.val.i64;
+                break;
+            case CDB_STR:
+                set_val[n_set].type         = DB_STR;
+                set_val[n_set].nul          = 0;
+                set_val[n_set].val.str_val  = pair->val.val.st;
+                break;
+            default:
+                LM_ERR("unsupported type %d for key %.*s\n", pair->val.type,
+                       pair->key.name.len, pair->key.name.s);
+                return -1;
+            }
+            
+            n_set++;
+        }
+    }
+    
+    n_equal = n_set;
+    
+	for (f = row_filter; f; f = f->next) {
+        if (n_filter >= MAX_NUM_COL) {
+            LM_BUG("Maximum number of filter supported is %d\n", MAX_NUM_COL);
+        }
+        
+        key[n_filter] = &f->key.name;
+        
+        switch(f->op) {
+        case CDB_OP_EQ:
+			op[n_filter] = OP_EQ;
+            
+            if (n_equal >= MAX_NUM_COL) {
+                LM_BUG("Maximum number of update & filter column supported is %d\n", MAX_NUM_COL);
+            }
+            
+            set_key[n_equal] = &f->key.name;
+            if (f->val.is_str) {
+                set_val[n_equal].type           = DB_STR;
+                set_val[n_equal].nul            = 0;
+                set_val[n_equal].val.str_val    = f->val.s;
+            } else {
+                set_val[n_equal].type           = DB_INT;
+                set_val[n_equal].nul            = 0;
+                set_val[n_equal].val.int_val    = f->val.i;
+            }
+            
+            n_equal++;
+            
+			break;
+		case CDB_OP_LT:
+			op[n_filter] = OP_LT;
+			break;
+		case CDB_OP_LTE:
+			op[n_filter] = OP_LEQ;
+			break;
+		case CDB_OP_GT:
+			op[n_filter] = OP_GT;
+			break;
+		case CDB_OP_GTE:
+			op[n_filter] = OP_GEQ;
+			break;
+		default:
+			LM_BUG("unsupported operator: %d\n", f->op);
+			return -1;
+        }
+        
+        if (f->val.is_str) {
+			val[n_filter].type          = DB_STR;
+            val[n_filter].nul           = 0;
+            val[n_filter].val.str_val   = f->val.s;
+		} else {
+			val[n_filter].type          = DB_INT;
+            val[n_filter].nul           = 0;
+            val[n_filter].val.int_val   = f->val.i;
+		}
+        
+        if (f->op != CDB_OP_EQ) {
+            is_complex_filter = 1;
+        }
+        
+        n_filter++;
+    }
+    
+    if (CACHEDBSQL_FUNC(con).insert_update(CACHEDBSQL_CON(con), set_key, set_val, n_equal) < 0) {
+		LM_ERR("failed to query database\n");
+		return -1;
+	}
+
+    // if there is complex op (different than = op) in filter, we have to run an extra update, otherwise, just a INSERT UPDATE above is enough
+    if (is_complex_filter) {
+        if (CACHEDBSQL_FUNC(con).update(CACHEDBSQL_CON(con), key, op, val, set_key, set_val, n_filter, n_set) < 0) {
+            LM_ERR("failed to query database\n");
+            return -1;
+        }
+    }
+    
+    return 0;
+}
+
 /**
  * init module function
  */
@@ -534,9 +926,13 @@ static int mod_init(void)
 	cde.cdb_func.get = dbcache_get;
 	cde.cdb_func.set = dbcache_set;
 	cde.cdb_func.remove = dbcache_remove;
+    cde.cdb_func._remove = _dbcache_remove;
 	cde.cdb_func.add = dbcache_add;
 	cde.cdb_func.sub = dbcache_sub;
 	cde.cdb_func.get_counter = dbcache_fetch_counter;
+    cde.cdb_func.query = dbcache_col_query;
+	cde.cdb_func.update = dbcache_col_update;
+	cde.cdb_func.truncate = dbcache_truncate;
 	cde.cdb_func.capability = 0;
 
 	if(cache_clean_period <= 0) {
